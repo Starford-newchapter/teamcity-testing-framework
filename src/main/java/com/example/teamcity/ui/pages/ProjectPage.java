6c0f9e6aaffb4040ecee90bfa4cf34e4a0d09fd9
v.kornilchik <v.kornilchik@tinkoff.ru>
2025-03-18 18:18:50 +0300

    added userSearchProject test

--- src/main/java/com/example/teamcity/ui/pages/ProjectPage.java
+++ src/main/java/com/example/teamcity/ui/pages/ProjectPage.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.$;
 
+
 public class ProjectPage extends  BasePage {
     private  static  final  String PROJECT_URL="/project/%s";
     public SelenideElement title = $("span[class*='ProjectPageHeader']");
