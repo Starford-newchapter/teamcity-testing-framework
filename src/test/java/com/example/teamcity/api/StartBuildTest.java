6cdcd1a9de3825afeec4e1fdfa663e125125c665
v.kornilchik <v.kornilchik@tinkoff.ru>
2025-04-11 14:36:23 +0300

    test Allure Report

--- src/test/java/com/example/teamcity/api/StartBuildTest.java
+++ src/test/java/com/example/teamcity/api/StartBuildTest.java
@@ -31,7 +31,6 @@ public class StartBuildTest extends BaseApiTest {
     }
 
 
-
     @Test(description = "User should be able to start build (with WireMock)", groups = {"Regression"}, alwaysRun = true)
     public void userStartsBuildWithWireMockTest() {
         var checkedBuildQueueRequest = new CheckedBase<>(Specifications.getSpec()
